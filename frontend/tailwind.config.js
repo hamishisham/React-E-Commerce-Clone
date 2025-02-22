6c63707407b79242d3944f5ab317b514870cb929
Muhammad <67207376+mohamedhesham221@users.noreply.github.com>
2025-02-22 12:03:53 +0200

    Revert "add/design login & fonts"

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,7 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {
-      fontFamily: {
-        IBM: '"IBM Plex Sans", serif',
-        inika: '"Inika", serif'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }

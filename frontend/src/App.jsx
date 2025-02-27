38df955a65131306768e5caabd645cede9ca53eb
Muhammad Hisham <mohmadetsh874@gmail.com>
2025-02-27 21:07:37 +0200

    Create/ checkout page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import Wishlist from "./pages/Wishlist";
 import CategoryProducts from "./pages/CategoryProducts";
 import NotFound from "./pages/NotFound";
 import Orders from "./pages/Orders";
+import Checkout from "./pages/Checkout";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/cart" element={<Cart />} />
         <Route path="*" element={<NotFound />}/>
         <Route path="/orders" element={<Orders />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/haneen" element={<Haneen />} />
         <Route path="/mohamed" element={<Mohamed />} />
--- frontend/src/test/Mohamed.jsx
+++ frontend/src/test/Mohamed.jsx
@@ -1,5 +1,5 @@
+
 const Mohamed = () => {
-	
 	return (
 		<>
 		</>
